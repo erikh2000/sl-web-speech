174dc71d9af9a5ff48315a56934bda5ca99dfa5a
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2023-05-03 19:53:27 -0700

    Added functions for return viseme text.

--- src/lipz/phonemeUtil.ts
+++ src/lipz/phonemeUtil.ts
@@ -1,9 +1,14 @@
 import ArpabetToBlair from "./ArpabetToBlair";
-import Viseme from './visemes';
+import Viseme, {visemeToText} from './visemes';
 
 export function phonemeToViseme(phoneme:string):Viseme {
   const viseme = ArpabetToBlair[phoneme];
   if (viseme !== undefined) return viseme;
   console.warn(`Unexpected "${phoneme}" phoneme could not be mapped to a viseme.`);
   return Viseme.REST;
+}
+
+export function phonemeToVisemeText(phoneme:string):string {
+  const viseme = phonemeToViseme(phoneme);
+  return visemeToText(viseme) ?? 'rest';
 }
--- src/lipz/visemes.ts
+++ src/lipz/visemes.ts
@@ -12,6 +12,21 @@ enum Viseme {
   COUNT
 };
 
+export function visemeToText(viseme:Viseme) {
+  switch(viseme) {
+    case Viseme.REST: return 'rest';
+    case Viseme.AI: return 'ai';
+    case Viseme.CONS: return 'cons';
+    case Viseme.E: return 'e';
+    case Viseme.FV: return 'fv';
+    case Viseme.L: return 'l';
+    case Viseme.O: return 'o';
+    case Viseme.MBP: return 'mbp';
+    case Viseme.U: return 'u';
+    case Viseme.WQ: return 'wq';
+  }
+}
+
 export default Viseme;
 
 // I gave up on exporting enums from the library. There doesn't seem to be a straightforward way to do it that doesn't
